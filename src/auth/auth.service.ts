ff945be9c1c79ef3b49b58fa8d6cce7596acd0ba
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-19 11:36:48 +0100

    feat(app): create db register and encrypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      // 1 - Encriptar contraseña
+
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user;
+    } catch (error) {
+      console.log(error.code);
+
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+      throw new InternalServerErrorException('Something terribe happen!!!');
+    }
   }
 
   findAll() {
