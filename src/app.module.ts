475b90fe179d41c0ed964607f172f0dbc8685893
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-19 09:12:49 +0100

    build(app): connect nest to mongo and create environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URI),
+
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+
+}
